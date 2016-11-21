95274d57193e1ac6ea5e24e7b6d2e127d2ee8fb5
Waholini <waholini@gmail.com>
2016-11-21 13:20:48 +0100

    More stuff

---
+++ webside.js
@@ -0,0 +1,17 @@
+var button = document.getElementById('ButtonElement');
+var overskrift = document.getElementById('overskriftElement');
+var image = document.getElementById('imageElement');
+
+
+
+button.onclick = function(){
+    alert("H");
+}
+
+overskrift.onclick = function(){
+    alert("Dette er en overskrift, ser du ikke det?");
+}
+
+image.onclick = function(){
+    alert("Meow.");
+}
